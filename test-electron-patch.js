36ed6ae03fefdbe2d770628d989d9520fc394095
TimSusa <tim.susa@teamit.fi>
2026-08-01 22:50:51 +0200

    chore: clean up test files, add to gitignore

--- test-electron-patch.js
+++
@@ -1,4 +0,0 @@
-// Test: does require('electron') work when entry point doesn't use it?
-console.log("typeof require('electron'):", typeof require("electron"));
-console.log("electron string:", require("electron"));
-process.exit(0);
